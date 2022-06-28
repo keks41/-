0f2a72eeb391d10af9c14db888351d28d4382054
geekbrains <igor_sharin@mail.ru>
2022-06-28 19:06:46 +0300

    %22

---
+++ Сеинар02/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Ведите первое число  ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i < numberA)
+{
+    if ( i % 2 == 0)
+    {
+        Console.Write(i + ", ");
+    }
+    i++;
+}
